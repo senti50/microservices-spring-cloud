a29a73b638ca5e46ce7d326a588f8b6289943d50
senti50 <blazej.pudlik@gmail.com>
2021-07-24 23:54:25 +0200

    added configuration from application properties

--- limits-service/src/main/java/pl/senti/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/pl/senti/limitsservice/controller/LimitsController.java
@@ -2,13 +2,20 @@ package pl.senti.limitsservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.senti.limitsservice.configuration.Configuration;
 import pl.senti.limitsservice.model.Limits;
 
 @RestController
 public class LimitsController {
 
+    private final Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
     public Limits getLimits(){
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
     }
 }
