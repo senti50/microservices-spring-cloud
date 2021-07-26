e2f2d0f6cd81cce6ddf8407eaa75ec0aeb4f4b54
senti50 <blazej.pudlik@gmail.com>
2021-07-26 22:47:35 +0200

    added currency conversion service and added Feign REST Client to invoke currency exchange service

---
+++ currency-conversion-service/src/main/java/pl/senti/currencyconversionservice/controller/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package pl.senti.currencyconversionservice.controller;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.senti.currencyconversionservice.model.CurrencyConversion;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversion getExchangeValue(@PathVariable String from, @PathVariable String to);
+}
---
+++ currency-conversion-service/src/test/java/pl/senti/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.senti.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
