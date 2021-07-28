53c0a96ec885bcd134e06c6c2c1f52adc4d2c987
senti50 <blazej.pudlik@gmail.com>
2021-07-28 22:31:45 +0200

    added client-side load balancing(removed url in proxy)

--- currency-conversion-service/src/main/java/pl/senti/currencyconversionservice/controller/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/pl/senti/currencyconversionservice/controller/CurrencyExchangeProxy.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pl.senti.currencyconversionservice.model.CurrencyConversion;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
